1d3361bce9f625fd0733db3072b03f87d681c05c
Sean McAfee <eefacm@gmail.com>
2017-01-31 09:53:10 -0800

    Check in new inoreader site file

---
+++ sites/inoreader.com.js
@@ -0,0 +1,14 @@
+const INOREADER_STYLE = "<style type='text/css'>" +
+      "#reader_pane code {" +
+      "display: inline; padding: 0; font-family: monospace;" +
+      "font-size: inherit; border: none; color: inherit;" +
+      "}</style>";
+
+$(INOREADER_STYLE).appendTo("head");
+
+$("body").keydown(function (e) {
+    if (inoreader_ignore_keydown_event(e))
+        e.stopImmediatePropagation();
+    if (e.keyCode == 83 && e.ctrlKey && e.shiftKey)
+        $(".article_current .footer_fav_img > a").clickthis();
+});
